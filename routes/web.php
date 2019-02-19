cfc9b2731837d27fb17a1356993cbcf50ca92997
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-02-19 04:11:38 -0430

    Initial routes added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+  'grades'    => 'GradeController',
+  'sections'  => 'SectionController',
+  'subjects'  => 'SubjectController',
+  'teachers'  => 'TeacherController',
+  'students'  => 'StudentController'
+]);
