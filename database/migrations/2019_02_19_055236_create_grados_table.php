9b1ea2447b6fb3458e449d0c8ec0b308b230a055
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-02-19 01:33:02 -0430

    Initial commit, model, migration, controller and factory created for Grado, Seccion and Asignatura

---
+++ database/migrations/2019_02_19_055236_create_grados_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGradosTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('grados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grados');
+    }
+}
